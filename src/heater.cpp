8a5bb75422888f2655fefd7045c2681d3a168bca
Vid Tadel <tadelv@gmail.com>
2024-08-13 06:52:49 +0200

    refactor heating

--- src/heater.cpp
+++ src/heater.cpp
@@ -10,6 +10,13 @@ void initHeater() {
 }
 
 static int heaterPower = 0;
+// Refresh period in milliseconds
+const unsigned long refreshPeriod = 1000; // 1 second (1000 milliseconds)
+
+// Variables to keep track of time
+unsigned long previousMillis = 0;
+unsigned long onTime = 0;
+bool isSSROn = false;
 
 void setHeaterPower(int power) {
 	if (power < 0) {
@@ -19,28 +26,35 @@ void setHeaterPower(int power) {
 	}
 	heaterPower = power;
 	Serial.printf("Heater power set to %d%%\n", power);
+	onTime = (refreshPeriod * heaterPower) / 100;
 }
 
-static int heaterPeriod = 2000;
 // Update the heater SSR state
 // where 100% is always turned on
 // while 10% is on 1/10 of the time
 // and the period is 1 second
 // so, if the power is 10%, the heater will be on for 100 ms and off for 900 ms
 void updateHeater() {
-	static unsigned long lastHeaterUpdate = 0;
-	if (millis() - lastHeaterUpdate >= heaterPeriod) {
-		lastHeaterUpdate = millis();
-		if (heaterPower == 0) {
-			digitalWrite(HEATER_PIN, LOW);
-		} else if (heaterPower == 100) {
-			digitalWrite(HEATER_PIN, HIGH);
-		} else {
-			if (millis() % heaterPeriod < heaterPower * heaterPeriod / 100) {
-				digitalWrite(HEATER_PIN, HIGH);
-			} else {
-				digitalWrite(HEATER_PIN, LOW);
-			}
-		}
-	}
+unsigned long currentMillis = millis();
+
+  // Check if the refresh period has passed
+  if (currentMillis - previousMillis >= refreshPeriod) {
+    // Save the last time the cycle started
+    previousMillis = currentMillis;
+
+    // Toggle the SSR state based on power percentage
+    if (heaterPower > 0) {
+      isSSROn = true;
+    } else {
+      isSSROn = false;
+    }
+  }
+
+  // Handle SSR on-time
+  if (isSSROn && (currentMillis - previousMillis <= onTime)) {
+    digitalWrite(HEATER_PIN, HIGH); // Turn SSR on
+  } else {
+    digitalWrite(HEATER_PIN, LOW); // Turn SSR off
+    isSSROn = false; // Ensure it's off if past onTime
+  }
 }
