19d69211f46e11c9cec8cd014da906f572a6af2f
Vid Tadel <tadelv@gmail.com>
2024-10-27 19:02:57 +0100

    wip

---
+++ webserver/src/components/HeaterSlider.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Slider from "@smui/slider";
+  import TextField from "@smui/textfield";
+  import Button from "@smui/button";
+  import { heaterPower, updateHeaterPower } from '../store.js';
+
+  function increment() { updateHeaterPower(heaterPower + 1); }
+  function decrement() { updateHeaterPower(heaterPower - 1); }
+</script>
+
+<div>
+  <Slider min="0" max="100" step="5" bind:value={$heaterPower} on:input={updateHeaterPower} />
+  <TextField label="Heater Power" type="number" bind:value={$heaterPower} readonly />
+  <Button on:click={increment}>+</Button>
+  <Button on:click={decrement}>-</Button>
+</div>
---
+++ webserver/src/components/SettingsDialog.svelte
@@ -0,0 +1,13 @@
+
+<script lang="ts">
+  import Dialog from "@smui/dialog";
+	import TextField from "@smui/textfield"
+  let show = false;
+</script>
+
+<Dialog bind:open={show}>
+  <h2>Settings</h2>
+  <TextField label="WiFi Name" />
+  <TextField label="WiFi Password" type="password" />
+  <!-- Additional placeholder fields for future settings -->
+</Dialog>
