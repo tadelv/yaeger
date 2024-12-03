c1b3d472a4526d2b4bf173b443bd4a247ab34058
Vid Tadel <tadelv@gmail.com>
2024-12-03 21:16:42 +0100

    different pins for s3mini

--- src/config.h
+++ src/config.h
@@ -1,3 +1,22 @@
+#ifdef S3MINI
+
+#define FAN_PIN 21
+
+#define HEATER_PIN 18
+
+// temp sensor pins
+#define MAX1DO 35
+#define MAX1CS 37
+#define MAX1CLK 33
+
+#define MAX2DO 35
+#define MAX2CS 39
+#define MAX2CLK 33
+
+#define DISPLAY_1 41
+#define DISPLAY_2 42
+
+#else
 
 #define FAN_PIN 8
 
@@ -11,6 +30,10 @@
 #define MAX2DO 5
 #define MAX2CS 16
 #define MAX2CLK 6
+
+#define DISPLAY_1 41
+#define DISPLAY_2 42
+#endif
 
 #define DEBUG
 #define EASY_AP
