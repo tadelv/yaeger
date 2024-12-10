961431dced45b24043a75dc1b645bcd44d31a9f9
Vid Tadel <tadelv@gmail.com>
2024-12-10 22:02:21 +0100

    formatting

--- webserver/src/App.svelte
+++ webserver/src/App.svelte
@@ -1,18 +1,25 @@
 <script>
-  import FanSlider from './components/FanSlider.svelte';
-  import HeaterSlider from './components/HeaterSlider.svelte';
-  import TemperatureReadout from './components/TemperatureReadout.svelte';
-  import RoastGraph from './components/RoastGraph.svelte';
-  import EventButtons from './components/EventButtons.svelte';
-  import SettingsDialog from './components/SettingsDialog.svelte';
-  import RoastSettingsDialog from './components/RoastSettingsDialog.svelte';
-  import { uploadRoast, startRoast, stopRoast, resetRoast, connectWebSocket, downloadRoast } from './store.ts';
-	import { onMount,  onDestroy } from 'svelte';
+  import FanSlider from "./components/FanSlider.svelte";
+  import HeaterSlider from "./components/HeaterSlider.svelte";
+  import TemperatureReadout from "./components/TemperatureReadout.svelte";
+  import RoastGraph from "./components/RoastGraph.svelte";
+  import EventButtons from "./components/EventButtons.svelte";
+  import SettingsDialog from "./components/SettingsDialog.svelte";
+  import RoastSettingsDialog from "./components/RoastSettingsDialog.svelte";
+  import {
+    uploadRoast,
+    startRoast,
+    stopRoast,
+    resetRoast,
+    connectWebSocket,
+    downloadRoast,
+  } from "./store.ts";
+  import { onMount, onDestroy } from "svelte";
 
   let showStart = true;
   let showStop = false;
   let showReset = false;
-  
+
   function startRoast1() {
     startRoast();
     showStart = false;
@@ -31,74 +38,79 @@
     showStart = true;
   }
 
-	let socket;
+  let socket;
 
-	onMount(() => {
-		console.log("connecting")
-		socket = connectWebSocket()
-		const fileInput = document.getElementById("fileInput");
-		console.log("file", fileInput)
-fileInput.addEventListener("change", (event) => {
-			console.log("change event: ", event)
-			const file = event.target.files[0]
-			if (!file) {
-				return
-			}
-			const reader = new FileReader()
+  onMount(() => {
+    console.log("connecting");
+    socket = connectWebSocket();
+    const fileInput = document.getElementById("fileInput");
+    console.log("file", fileInput);
+    fileInput.addEventListener("change", (event) => {
+      console.log("change event: ", event);
+      const file = event.target.files[0];
+      if (!file) {
+        return;
+      }
+      const reader = new FileReader();
 
-			reader.onload = (e) => {
-				try {
-					console.log("reading: ", e.target.result)
-					const jsonData = JSON.parse(e.target.result)
-					uploadRoast(jsonData)
-				} catch (error) {
-					console.log("upload failed:", error)
-				}
-			}
-			reader.readAsText(file)
-		})
-	})
+      reader.onload = (e) => {
+        try {
+          console.log("reading: ", e.target.result);
+          const jsonData = JSON.parse(e.target.result);
+          uploadRoast(jsonData);
+        } catch (error) {
+          console.log("upload failed:", error);
+        }
+      };
+      reader.readAsText(file);
+    });
+  });
 
-	onDestroy(() => {
-		console.log("destroying")
-		socket?.close()
-	})
+  onDestroy(() => {
+    console.log("destroying");
+    socket?.close();
+  });
 
-	function download() {
-		const blob = new Blob([downloadRoast()], { type: "application/json" })
-		const url = URL.createObjectURL(blob)
+  function download() {
+    const blob = new Blob([downloadRoast()], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
 
-		const a = document.createElement("a")
-		a.href = url
-		a.download = "roast.json"
-		a.click()
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "roast.json";
+    a.click();
 
-		URL.revokeObjectURL(url)
-	}
+    URL.revokeObjectURL(url);
+  }
 
-	function upload() {
-		const fileInput = document.getElementById("fileInput");
-		fileInput.click()
-	}
+  function upload() {
+    const fileInput = document.getElementById("fileInput");
+    fileInput.click();
+  }
 </script>
 
 <div>
   {#if showStart}
     <button on:click={startRoast1}>Start</button>
-		<button on:click={upload}>Upload</button>
+    <button on:click={upload}>Upload</button>
   {:else if showStop}
     <button on:click={stopRoast1}>Stop</button>
   {:else if showReset}
     <button on:click={reset}>Reset</button>
-		<button on:click={download}>Download</button>
+    <button on:click={download}>Download</button>
   {/if}
 
   <FanSlider />
   <HeaterSlider />
-	<TemperatureReadout />
+  <TemperatureReadout />
   <RoastGraph />
   <EventButtons />
   <SettingsDialog />
   <RoastSettingsDialog />
-	<input type="file" id="fileInput" accept="application/json" style="display: none;">
+  <input
+    type="file"
+    id="fileInput"
+    accept="application/json"
+    style="display: none;"
+  />
 </div>
--- webserver/src/components/RoastGraph.svelte
+++ webserver/src/components/RoastGraph.svelte
@@ -1,144 +1,143 @@
-
 <script lang="ts">
-	import { onMount } from 'svelte'
-  import Chart from 'chart.js/auto';
-	import chartTrendline from 'chartjs-plugin-trendline';
-	import 'chartjs-adapter-date-fns';
-  import { readings, events, roastStart } from '../store.ts';
-	import { get } from 'svelte/store';
+  import { onMount } from "svelte";
+  import Chart from "chart.js/auto";
+  import chartTrendline from "chartjs-plugin-trendline";
+  import "chartjs-adapter-date-fns";
+  import { readings, events, roastStart } from "../store.ts";
+  import { get } from "svelte/store";
 
-	const verticalLinePlugin = {
-		 getLinePosition: function (chart, pointIndex) {
-				 const meta = chart.getDatasetMeta(0); // first dataset is used to discover X coordinate of a point
-				 const data = meta.data;
-				 return data[pointIndex.idx].x;
-		 },
+  const verticalLinePlugin = {
+    getLinePosition: function (chart, pointIndex) {
+      const meta = chart.getDatasetMeta(0); // first dataset is used to discover X coordinate of a point
+      const data = meta.data;
+      return data[pointIndex.idx].x;
+    },
 
-		 renderVerticalLine: function (chartInstance, pointIndex) {
-				 const lineLeftOffset = this.getLinePosition(chartInstance, pointIndex);
-				 const scale = chartInstance.scales.y;
-				 const context = chartInstance.ctx;
-				 // render vertical line
-				 context.beginPath();
-				 context.strokeStyle = '#ff0000';
-				 context.moveTo(lineLeftOffset, scale.top);
-				 context.lineTo(lineLeftOffset, scale.bottom);
-				 context.stroke();
+    renderVerticalLine: function (chartInstance, pointIndex) {
+      const lineLeftOffset = this.getLinePosition(chartInstance, pointIndex);
+      const scale = chartInstance.scales.y;
+      const context = chartInstance.ctx;
+      // render vertical line
+      context.beginPath();
+      context.strokeStyle = "#ff0000";
+      context.moveTo(lineLeftOffset, scale.top);
+      context.lineTo(lineLeftOffset, scale.bottom);
+      context.stroke();
 
-				 // write label
-				 context.fillStyle = "#ff0000";
-				 context.textAlign = 'center';
-				 context.fillText(pointIndex.label, lineLeftOffset, (scale.bottom - scale.top) / 2 + scale.top);
-		 },
+      // write label
+      context.fillStyle = "#ff0000";
+      context.textAlign = "center";
+      context.fillText(
+        pointIndex.label,
+        lineLeftOffset,
+        (scale.bottom - scale.top) / 2 + scale.top,
+      );
+    },
 
-		 beforeDatasetsDraw: function (chart, easing) {
-				if(chart.config._config.lineAtIndex) {
-				console.log("doing ", chart.config._config.lineAtIndex)
-				  chart.config._config.lineAtIndex.forEach(pointIndex => {
-					  this.renderVerticalLine(chart, pointIndex)
-				  })
-				}
-		 }
-	};
+    beforeDatasetsDraw: function (chart, easing) {
+      if (chart.config._config.lineAtIndex) {
+        console.log("doing ", chart.config._config.lineAtIndex);
+        chart.config._config.lineAtIndex.forEach((pointIndex) => {
+          this.renderVerticalLine(chart, pointIndex);
+        });
+      }
+    },
+  };
 
   let canvas;
 
-
-
-	const data = {
-		labels: [
-		],
-        datasets: [
-			{ 
-				label: 'Bean Temp', 
-				borderColor: 'blue', 
-				data: [],
-				trendlineLinear: {
-					colorMin: '#0f0588',
-					colorMax: '#00f0ff',
-					lineStyle: 'dotted',
-					width: 1
-				}
-			},
-          { 
-				label: 'Exhaust Temp', 
-				borderColor: 'red', 
-				data: [],
-				trendlineLinear: {
-					colorMin: '#666099',
-					colorMax: '#f770aa',
-					lineStyle: 'dotted',
-					projection: false,
-					width: 3
-				}
-
-			},
-          {
-				label: 'Fan Power',
-				borderColor: '#055088', 
-				data: [],
-			},
-          { label: 'Heater Power', borderColor: 'orange', data: [] }
-        ]
-      }
+  const data = {
+    labels: [],
+    datasets: [
+      {
+        label: "Bean Temp",
+        borderColor: "blue",
+        data: [],
+        trendlineLinear: {
+          colorMin: "#0f0588",
+          colorMax: "#00f0ff",
+          lineStyle: "dotted",
+          width: 1,
+        },
+      },
+      {
+        label: "Exhaust Temp",
+        borderColor: "red",
+        data: [],
+        trendlineLinear: {
+          colorMin: "#666099",
+          colorMax: "#f770aa",
+          lineStyle: "dotted",
+          projection: false,
+          width: 3,
+        },
+      },
+      {
+        label: "Fan Power",
+        borderColor: "#055088",
+        data: [],
+      },
+      { label: "Heater Power", borderColor: "orange", data: [] },
+    ],
+  };
   onMount(() => {
-		console.log('mount')
-    const ctx = canvas.getContext('2d');
+    console.log("mount");
+    const ctx = canvas.getContext("2d");
     const chart = new Chart(ctx, {
-      type: 'line',
+      type: "line",
       data: data,
       options: {
         scales: {
-					x: { grace: 20, type: 'linear', bounds: 'ticks', beginAtZero: true },
+          x: { grace: 20, type: "linear", bounds: "ticks", beginAtZero: true },
           //x: { type: 'time', time: { unit: 'minute' } },
-          y: { min: 0, max: 250 }
+          y: { min: 0, max: 250 },
         },
-				responsive: true,
-				animation: false,
-				//    layout: {
-				//	padding: {
-				//		right: 250
-				//	}
-				//}
+        responsive: true,
+        animation: false,
+        //    layout: {
+        //	padding: {
+        //		right: 250
+        //	}
+        //}
       },
-			lineAtIndex: [],
-			plugins: [verticalLinePlugin, chartTrendline]
+      lineAtIndex: [],
+      plugins: [verticalLinePlugin, chartTrendline],
     });
-
 
     readings.subscribe((inData) => {
       // Update chart data dynamically
-			var lastRead = inData[inData.length - 1]
-			console.log('last read:', lastRead)
-			if (lastRead == undefined) {
-				console.log('resetting')
-				chart.data.labels = []
-				chart.data.datasets.forEach((dataset) => {
-					dataset.data = []
-				})
-				chart.config._config.lineAtIndex = []
-				chart.update()
-			} else {
-			chart.data.datasets[1].data.push(lastRead.ET)
-			chart.data.datasets[0].data.push(lastRead.BT)
-			chart.data.datasets[2].data.push(lastRead.FanVal);
-			chart.data.datasets[3].data.push(lastRead.BurnerVal);
-			
-			
-			data.labels.push(`${Math.floor(new Date().getTime() / 1000 - get(roastStart))}`)
-			chart.update()
-			}
+      var lastRead = inData[inData.length - 1];
+      console.log("last read:", lastRead);
+      if (lastRead == undefined) {
+        console.log("resetting");
+        chart.data.labels = [];
+        chart.data.datasets.forEach((dataset) => {
+          dataset.data = [];
+        });
+        chart.config._config.lineAtIndex = [];
+        chart.update();
+      } else {
+        chart.data.datasets[1].data.push(lastRead.ET);
+        chart.data.datasets[0].data.push(lastRead.BT);
+        chart.data.datasets[2].data.push(lastRead.FanVal);
+        chart.data.datasets[3].data.push(lastRead.BurnerVal);
+
+        data.labels.push(
+          `${Math.floor(new Date().getTime() / 1000 - get(roastStart))}`,
+        );
+        chart.update();
+      }
     });
 
-		events.subscribe(((inEvents) => {
-			console.log("got ", inEvents)
-			chart.config._config.lineAtIndex = inEvents
-			console.log(chart.lineAtIndex)
-		}))
-//startRoast()
+    events.subscribe((inEvents) => {
+      console.log("got ", inEvents);
+      chart.config._config.lineAtIndex = inEvents;
+      console.log(chart.lineAtIndex);
+    });
+    //startRoast()
   });
 </script>
 
 <div>
-<canvas bind:this={canvas}></canvas>
+  <canvas bind:this={canvas}></canvas>
 </div>
