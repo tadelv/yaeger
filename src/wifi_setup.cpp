5e8c6cd8395efe7d6d53e855992bbb6b033cbbe8
Vid Tadel <tadelv@gmail.com>
2025-01-28 20:38:31 +0100

    fix wifi switch from STA to AP

--- src/wifi_setup.cpp
+++ src/wifi_setup.cpp
@@ -1,19 +1,21 @@
+#include "WiFiType.h"
+#include "esp32-hal.h"
+#include "logging.h"
 #include <Arduino.h>
 #include <ESPmDNS.h>
 #include <Preferences.h>
 #include <WiFi.h>
-#include "logging.h"
 
 const char *wifiPrefsKey = "wifi";
 const char *wifiSSIDKey = "ssid";
 const char *wifiPassKey = "pass";
-
 
 class WiFiParams {
 private:
   String ssid = "";
   String pass = "";
   Preferences preferences;
+
 public:
   String getSSID() { return ssid; }
   String getPass() { return pass; }
@@ -25,8 +27,9 @@ public:
 
 WiFiParams params;
 
-
 void setupAP() {
+  WiFi.mode(WIFI_AP);
+  delay(100);
   WiFi.softAP("Yaeger");
   WiFi.setTxPower(WIFI_POWER_8_5dBm);
 }
@@ -38,16 +41,18 @@ void connectToWifi() {
   WiFi.setTxPower(WIFI_POWER_8_5dBm);
   int wifiCounter = 0;
   while (WiFi.status() != WL_CONNECTED) {
-		if (WiFi.status() == WL_CONNECT_FAILED) {
-		  log("Connect failed, restoring AP");
-			setupAP();
-			break;
-		}
+    if (WiFi.status() == WL_CONNECT_FAILED) {
+      log("Connect failed, restoring AP");
+      setupAP();
+      break;
+    }
     wifiCounter++;
     delay(1000);
     log(".");
     if (wifiCounter > 10) {
-			setupAP();
+      WiFi.disconnect(true);
+      delay(100);
+      setupAP();
       break;
     }
   }
@@ -61,18 +66,18 @@ void connectToWifi() {
 void setupWifi() {
   // TODO: blink led
   //
-	
-	params.init();
+
+  params.init();
 
   const char *hostname = "yaeger.local";
   WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
   WiFi.setHostname(hostname);
 
   if (params.hasCredentials()) {
-		log("trying to connect to wifi");
-		connectToWifi();
+    log("trying to connect to wifi");
+    connectToWifi();
   } else {
-		log("no wifi data found, setting up AP");
+    log("no wifi data found, setting up AP");
     setupAP();
   }
 
@@ -81,14 +86,14 @@ void setupWifi() {
   }
 }
 
-
 // ----------------------------------------------------
 // ------------------ WiFiParams ----------------------
 // ----------------------------------------------------
 
 // TODO: use this
 void WiFiParams::saveCredentials(String ssid, String pass) {
-  if (this->ssid == ssid && this->pass == pass) return;
+  if (this->ssid == ssid && this->pass == pass)
+    return;
 
   this->ssid = ssid;
   this->pass = pass;
